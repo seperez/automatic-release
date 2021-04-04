264a8f9a1308cfc1d8ba0855f934a0a9210e79a7
Sebastián M. Pérez <perezsebastianm@gmail.com>
2021-04-04 02:27:34 -0300

    feat: automatic changelog (#2)

---
+++ changelog.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+ 
+  fileName: "CHANGELOG.md",
+ 
+  dateFormat: "dd/LL/yyyy",
+ 
+  separator: "-",
+ 
+  triggers: [
+    "🐛 Bug",
+    "feat",
+    "💥 Change",
+  ],
+ 
+  pluraliseTrigger: true,
+  
+  skipEmptyCommitMessages: false,
+ 
+  customMessageFormatter: null,
+  
+  customHeadingFormatter: null,
+ 
+};
